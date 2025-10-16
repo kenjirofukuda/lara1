8b4e0d86cb28027d82b468f31cb17ed324ca4bcd
kenjirofukuda <kenjirofukuda@gmail.com>
2025-10-17 03:00:33 +0900

    more better way. use foreignIdFor

--- database/migrations/2025_10_15_225729_create_job_listings_table.php
+++ database/migrations/2025_10_15_225729_create_job_listings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,8 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('employer_id');
+            // $table->unsignedBigInteger('employer_id');
+            $table->foreignIdFor(App\Models\Job::class);
             $table->string('title');
             $table->string('salary');
             $table->timestamps();
